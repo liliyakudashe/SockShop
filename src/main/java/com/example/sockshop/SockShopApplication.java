5778f42a8bd76c55f570f7d0e1f41a80a5e5532f
Acer <oduvan4ik_888@mail.ru>
2023-01-15 12:00:15 +0300

    Initial commit

---
+++ src/main/java/com/example/sockshop/SockShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sockshop/SockShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
